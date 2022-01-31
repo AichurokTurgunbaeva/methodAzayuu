e3fd62681ab522e0b10bb2a010027c2448447e49
Notnik_KG <tasnim0800@gmail.com>
2022-01-31 15:33:30 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        method(15);
+    }
+    static void method (int a) {
+        int schetchik = 0;
+        for (int i = 0; i<=a; i++) {
+            schetchik=a-i;
+            System.out.print(schetchik+ " ");
+
+        }
+    }
+    }
+
